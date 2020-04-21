a6844b4a7e393449a63e2ed8ba25acdffe661e0e
sikth 394 <avivw08@gmail.com>
2020-04-21 19:19:29 +0300

    first commit

---
+++ Assets/Scripts/RotateCameraModified.cs
@@ -0,0 +1,147 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class RotateCameraModified : MonoBehaviour
+{
+    public float lookLeftMax = -8.651f;
+    public float lookRightMax = 7.651f;
+    Quaternion resetCamera;
+   
+    //Can Use Touch
+    bool isTouchDevice;
+
+    //Speed
+    float arrowMouseSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            isTouchDevice = true;
+        }
+        else
+        {
+            isTouchDevice = false;
+        }
+
+      
+        
+
+        //Get local rotation
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouchDevice)
+        {
+            //Rotate Camera with Accelerometer
+           cameraRotationAccelerometer();
+        }
+        else
+        {
+            //Rotate Camera with Keyboard Arrow and Mouse
+            moveWithArrowAndMouse();
+        }
+
+        if(!(Input.GetKey(KeyCode.LeftArrow)))
+        {
+            if (transform.rotation.y < 0.0f)
+            {
+                moveCamera(1f, 0.0f, 2.0f);
+            }
+        }
+        if (!(Input.GetKey(KeyCode.RightArrow)))
+        {
+            if (transform.rotation.y > 0.0f)
+            {
+                moveCamera(-1.0f, 0.0f, 2.0f);
+            }
+        }
+
+
+    }
+
+    /////////////**** Accelerometer Start****////////////////////////////////////////////////////////
+    float xValue;
+    float xValueMinMax = 5.0f;
+
+    float yValue;
+    float yValueMinMax = 5.0f;
+
+    float cameraSpeed = 20.0f;// Greater the lower speed
+    Vector3 accelometerSmoothValue;
+
+    void cameraRotationAccelerometer()
+    {
+        //Set X Min Max
+        if (xValue < -xValueMinMax)
+            xValue = -xValueMinMax;
+
+        if (xValue > xValueMinMax)
+            xValue = xValueMinMax;
+
+        //Set Y Min Max
+        if (yValue < -yValueMinMax)
+            yValue = -yValueMinMax;
+
+        if (yValue > yValueMinMax)
+            yValue = yValueMinMax;
+
+        accelometerSmoothValue = lowpass();
+
+        xValue += accelometerSmoothValue.x;
+        yValue += accelometerSmoothValue.y;
+
+        transform.rotation = new Quaternion(yValue, xValue, 0, cameraSpeed);
+    }
+
+    //Smooth Accelerometer
+    public float AccelerometerUpdateInterval = 1.0f / 100.0f;
+    public float LowPassKernelWidthInSeconds = 0.001f;
+    public Vector3 lowPassValue = Vector3.zero;
+    Vector3 lowpass()
+    {
+        float LowPassFilterFactor = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;//tweakable
+        lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+        return lowPassValue;
+    }
+    /////////////**** Accelerometer End****////////////////////////////////////////////////////////
+
+    //Move with Keyboard Arrow
+    void moveWithArrowAndMouse()
+    {
+        //Keyboard Arrow
+        moveCamera(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), arrowMouseSpeed);
+
+        //Keyboard Mouse
+        //moveCamera(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), arrowMouseSpeed);
+    }
+
+    //Move Parameters
+    float mouseX;
+    float mouseY;
+    Quaternion localRotation;
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    void moveCamera(float horizontal, float verticle, float moveSpeed)
+    {
+        mouseX = horizontal;
+        mouseY = -verticle;
+
+        rotY += mouseX * moveSpeed/10;
+        rotX += mouseY * moveSpeed/10;
+
+        rotY = Mathf.Clamp(rotY, lookLeftMax, lookRightMax);
+        localRotation = Quaternion.Euler(0.0f, rotY, -rotY);
+        transform.rotation = localRotation;
+    }
+
+    
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource[] Farts;
+    public AudioSource[] Girl;
+    float startTime = 0f;
+    float holdTime = 2.7f;
+    int indexF;
+    int indexG;
+    bool isPlaying;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("girlTalk", 6.0f, 12.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.LeftArrow)) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            startTime = Time.time;
+            foreach (AudioSource fart in Farts)
+            {
+                if (fart.isPlaying)
+                {
+                    isPlaying = true;
+                    break;
+                }
+                else
+                {
+                    isPlaying = false;
+                }
+            }
+        }
+        if (((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.RightArrow))) && !isPlaying)
+        {
+            
+            if (startTime + holdTime <= Time.time)
+            {
+                isPlaying = true;
+                indexF = UnityEngine.Random.Range(0, Farts.Length);
+                Farts[indexF].Play();
+            }
+        }
+        
+    }
+
+    public void girlTalk()
+    {
+        indexG = UnityEngine.Random.Range(0, Girl.Length);
+        Girl[indexG].Play();
+    }
+
+    
+}
